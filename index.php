f828dbe7494c7e1ac6f5bab62e0bacb895acf173
pragati shirsat <72141551+123456-lgtm-do@users.noreply.github.com>
2021-06-05 23:43:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ session_start();
 
   <head>
     <title> Home | Le Cafe' </title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CFDB92WQEC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CFDB92WQEC');
+</script>
   </head>
 
   <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
