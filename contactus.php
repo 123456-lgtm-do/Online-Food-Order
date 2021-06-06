b69aae065bf92b5b4468e89bc80aa18147bac528
pragati shirsat <72141551+123456-lgtm-do@users.noreply.github.com>
2021-06-07 00:25:44 +0530

    Add files via upload

--- contactus.php
+++ contactus.php
@@ -189,6 +189,17 @@ if (!$success){
 $conn->close();
 }
 ?>
+<center>
+		<h1 style="color:lawngreen;">
+			
+		</h1>
+		
+		<div>
+			<!-- Google Map Copied Code -->
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7968.501324911743!2d101.738321!3d3.027247!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xed0cc431806c9ec8!2sLe%20Gardenz%20Cafe!5e0!3m2!1sen!2smy!4v1622913092930!5m2!1sen!2smy" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+			
+		</div>
+	</center>
 
      </body>
 
